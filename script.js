31a08f2dd069c72dfdb0de9f34624e9059eab3fb
KrishP69 <krish.patil@vit.edu.in>
2026-03-11 23:36:45 +0530

    Improved UI design

--- script.js
+++ script.js
@@ -10,76 +10,65 @@ e.preventDefault();
 
 dropArea.addEventListener("drop", function(e){
 e.preventDefault();
-
-let file = e.dataTransfer.files[0];
 fileInput.files = e.dataTransfer.files;
-
 showImage();
 });
 
 function showImage(){
-
 let file = fileInput.files[0];
-
 if(!file) return;
 
 preview.src = URL.createObjectURL(file);
 preview.style.display = "block";
-
 }
 
 function removeImage(){
-
-fileInput.value = "";
-preview.src = "";
-preview.style.display = "none";
-
-document.getElementById("result").innerText = "";
-document.getElementById("confidenceBar").style.display = "none";
-
+fileInput.value="";
+preview.src="";
+preview.style.display="none";
+document.getElementById("result").innerText="";
+document.getElementById("confidenceBar").style.display="none";
 }
 
 async function detectAI(){
 
-if(fileInput.files.length === 0){
+if(fileInput.files.length===0){
 alert("Upload an image first");
 return;
 }
 
-let result = document.getElementById("result");
-let loading = document.getElementById("loading");
-let bar = document.getElementById("confidenceBar");
-let fill = document.getElementById("confidenceFill");
+let result=document.getElementById("result");
+let loading=document.getElementById("loading");
+let bar=document.getElementById("confidenceBar");
+let fill=document.getElementById("confidenceFill");
 
-result.innerText = "";
-loading.style.display = "block";
+loading.style.display="block";
+result.innerText="";
 
 try{
 
-let formData = new FormData();
-formData.append("image", fileInput.files[0]);
+let formData=new FormData();
+formData.append("image",fileInput.files[0]);
 
-let response = await fetch(
+let response=await fetch(
 "https://ai-detector-api-q80d.onrender.com/detect",
 {
 method:"POST",
 body:formData
 });
 
-let data = await response.json();
+let data=await response.json();
 
-loading.style.display = "none";
+loading.style.display="none";
 
-result.innerText =
-data.result + " (" + data.confidence + "% confidence)";
-
-bar.style.display = "block";
-fill.style.width = data.confidence + "%";
+result.innerText=data.result+" ("+data.confidence+"% confidence)";
+bar.style.display="block";
+fill.style.width=data.confidence+"%";
 
 }catch(error){
 
-loading.style.display = "none";
-result.innerText = "Error connecting to server";
+loading.style.display="none";
+result.innerText="Server error";
 
 }
 
