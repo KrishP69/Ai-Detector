d733a8fb6dad57f9363a72dcff18b16b386bcc58
KrishP69 <krish.patil@vit.edu.in>
2026-03-12 01:24:33 +0530

    Clean UI removed forensic preview

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let fileInput = document.getElementById("imageUpload")
 let preview = document.getElementById("preview")
-let forensic = document.getElementById("forensicImage")
 let dropArea = document.getElementById("dropArea")
 
 fileInput.addEventListener("change", showImage)
@@ -24,8 +23,6 @@ if(!file) return
 preview.src = URL.createObjectURL(file)
 preview.style.display = "block"
 
-forensic.style.display = "none"
-
 }
 
 function removeImage(){
@@ -33,9 +30,6 @@ function removeImage(){
 fileInput.value=""
 preview.src=""
 preview.style.display="none"
-
-forensic.src=""
-forensic.style.display="none"
 
 document.getElementById("result").innerText=""
 document.getElementById("confidenceBar").style.display="none"
@@ -78,11 +72,6 @@ result.innerText=data.result+" ("+data.confidence+"% confidence)"
 
 bar.style.display="block"
 fill.style.width=data.confidence+"%"
-
-forensic.src =
-"https://ai-detector-api-q80d.onrender.com/forensic?"+Date.now()
-
-forensic.style.display = "block"
 
 let aiScore=data.confidence
 
@@ -149,74 +138,3 @@ toggle.innerText="🌙"
 }
 
 }
-
-
-/* Zoom viewer */
-
-let zoomModal=document.getElementById("zoomModal")
-let zoomImage=document.getElementById("zoomImage")
-let closeZoom=document.getElementById("closeZoom")
-
-let scale=1
-let posX=0
-let posY=0
-let dragging=false
-let startX,startY
-
-forensic.onclick=function(){
-
-zoomModal.style.display="flex"
-zoomImage.src=forensic.src
-
-scale=1
-posX=0
-posY=0
-
-zoomImage.style.transform=`scale(${scale}) translate(${posX}px,${posY}px)`
-
-}
-
-closeZoom.onclick=function(){
-zoomModal.style.display="none"
-}
-
-zoomImage.addEventListener("wheel",function(e){
-
-e.preventDefault()
-
-scale+=e.deltaY*-0.001
-
-scale=Math.min(Math.max(.5,scale),5)
-
-zoomImage.style.transform=`scale(${scale}) translate(${posX}px,${posY}px)`
-
-})
-
-zoomImage.addEventListener("mousedown",function(e){
-
-dragging=true
-
-startX=e.clientX-posX
-startY=e.clientY-posY
-
-zoomImage.style.cursor="grabbing"
-
-})
-
-window.addEventListener("mouseup",()=>{
-
-dragging=false
-zoomImage.style.cursor="grab"
-
-})
-
-window.addEventListener("mousemove",function(e){
-
-if(!dragging) return
-
-posX=e.clientX-startX
-posY=e.clientY-startY
-
-zoomImage.style.transform=`scale(${scale}) translate(${posX}px,${posY}px)`
-
-})
